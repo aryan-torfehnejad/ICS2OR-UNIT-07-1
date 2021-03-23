10053775c46bf615d58f655a66bd9c87b5310103
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 18:19:13 +0000

    fixed again

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-  document.getElementById('button').addEventListener
+document.getElementById('button').addEventListener
 ('click',randomizer)
 let randomNumber = 4
 let userGuess = 4
 function randomizer() { 
-  userGuess = document.getElementById('input').value
-  userGuess =parseInt(userGuess)
+userGuess = document.getElementById('input').value
+userGuess =parseInt(userGuess)
 // generate random number between 1 and 6
-  randomNumber = (Math.random() * 6) + 1
-  randomNumber = parseInt(randomNumber)
+randomNumber = (Math.random() * 6) + 1
+randomNumber = parseInt(randomNumber)
 // compare user's guess with random number
-  if (userGuess === randomNumber) {
-    document.getElementById('answer').innerHTML =
-      "You win a cookie!"
-  }
+if (userGuess = = = randomNumber) {
+document.getElementById ('answer').innerHTML =
+"You win a cookie!"
+}
 }
