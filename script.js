ab444ffdfe6b4b893efa2de34b88ab67c8a81c03
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 17:45:27 +0000

    fixed

--- script.js
+++ script.js
@@ -2,18 +2,19 @@
 ('click',randomizer)
 let randomNumber = 4
 let userGuess = 4
-function randomizer() {
-  //get number typed in by user and convert to an integer
+
+function randomizer() { 
   userGuess = document.getElementById('input').value
   userGuess =parseInt(userGuess)
 
-  //generate random number between 1 and 6
+  // generate random number between 1 and 6
   randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
 
-  //compare user's guess with random number
+  // compare user's guess with random number
   if (userGuess === randomNumber) {
     document.getElementById('answer').innerHTML =
       "You win a cookie!"
   }
 }
+
