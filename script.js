09064afa8d266cb4e63aa04d6cd40f5b55d3975e
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 18:07:30 +0000

    fixed html errors

--- script.js
+++ script.js
@@ -2,19 +2,15 @@
 ('click',randomizer)
 let randomNumber = 4
 let userGuess = 4
-
 function randomizer() { 
   userGuess = document.getElementById('input').value
   userGuess =parseInt(userGuess)
-
-  // generate random number between 1 and 6
+// generate random number between 1 and 6
   randomNumber = (Math.random() * 6) + 1
   randomNumber = parseInt(randomNumber)
-
-  // compare user's guess with random number
+// compare user's guess with random number
   if (userGuess === randomNumber) {
     document.getElementById('answer').innerHTML =
       "You win a cookie!"
   }
 }
-
