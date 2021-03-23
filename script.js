65c6ec36ff49fe42b31bc3a33bbc81d4970fbb90
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 16:34:30 +0000

    fixed spaces

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 document.getElementById('button').addEventListener
-('click',randomizer)
- let randomNumber=4
- let userGuess=4
+  ('click', randomizer)
+let randomNumber = 4
+let userGuess = 4
 
-
-function randomizer(){
+function randomizer() {
   //get number typed in by user and convert to an integer
-  userGuess=document.getElementById('input').value
-  userGuess=parseInt(userGuess)
+  userGuess = document.getElementById('input').value
+  userGuess = parseInt(userGuess)
 
-//generate random number between 1 and 6
-  randomNumber= (Math.random()* 6) + 1
-  randomNumber=parseInt (randomNumber)
+  //generate random number between 1 and 6
+  randomNumber = (Math.random() * 6) + 1
+  randomNumber = parseInt(randomNumber)
 
-// compare user's guess with random number
-  if(userGuess==randomNumber){
-  document.getElementById('answer').innerHTML=
-  "You win a cookie!"
-}
+  // compare user's guess with random number
+  if (userGuess == randomNumber) {
+    document.getElementById('answer').innerHTML =
+      "You win a cookie!"
+  }
 }
