c022bb750c045e1331fea3ee6505ca6dbceb0730
aryantorfehneja <aryan.torfeh-nejad@ocsbstudent.ca>
2021-03-23 18:24:30 +0000

    fixed again

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let randomNumber = 4
 let userGuess = 4
 function randomizer() { 
 userGuess = document.getElementById('input').value
-userGuess =parseInt(userGuess)
+userGuess = parseInt(userGuess)
 // generate random number between 1 and 6
 randomNumber = (Math.random() * 6) + 1
 randomNumber = parseInt(randomNumber)
